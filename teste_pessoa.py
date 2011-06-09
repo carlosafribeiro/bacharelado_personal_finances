f14964e0772d6b24f1144f1b29ff4c1cabf4423a
Carlos Alexandre Ferreira Ribeiro <carlos.alefr@gmail.com>
2011-06-09 00:41:29 -0300

    Updated classes

--- teste_pessoa.py
+++ teste_pessoa.py
@@ -3,6 +3,7 @@
 import unittest
 from should_dsl import should
 from pessoa import Pessoa
+from gasto import Gasto
 
 class TestPessoa(unittest.TestCase):
 
@@ -12,9 +13,7 @@ class TestPessoa(unittest.TestCase):
         pessoa.salario |should| equal_to(1000)
         pessoa.data_pagamento |should| equal_to(10)
         
-    def teste_mostrar_gastos(self):
-        pessoa = Pessoa("Maria", 1000, 10)
-          
+         
         
 if __name__=="__main__":
     unittest.main()
